e72e511ba5ead5ff7c66be3f4297fd15b6831538
NaimulSakib <naimulsakib.88@gmail.com>
2021-02-24 01:23:53 +0600

    background

---
+++ js.js
@@ -0,0 +1,30 @@
+
+function bmi(){
+    var h = document.getElementById('h').value;
+    var w = document.getElementById('w').value;
+  var bmi = w/(h/100*h/100);
+  var bmio=(bmi.toFixed(2));
+  
+  document.getElementById('r').innerHTML="Your BMI is: "+bmio;
+  if(bmio<18.5)
+  {
+  document.getElementById('r').innerHTML="Your BMI is: "+bmio+" Under weight";
+  }
+
+   else if(bmio>18.5 && bmio<24.5)
+   {
+   document.getElementById('r').innerHTML="Your BMI is: "+bmio+" Normal weight";
+   }
+else if(bmio>24.5 && bmio<29.5)
+{
+document.getElementById('r').innerHTML="Your BMI is: "+bmio+" Over weight";
+}
+ else if(bmio>29.5 && bmio<34.5)
+{
+document.getElementById('r').innerHTML="Your BMI is: "+bmio+" Obese weight";
+}
+  else if(bmio>34.5)
+{
+document.getElementById('r').innerHTM="Your BMI is: "+bmio+" Extremely Obese";
+}
+  }
